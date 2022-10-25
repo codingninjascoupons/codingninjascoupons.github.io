9b04a70e7891e3f235f039f5e5c7034308f1ca7a
HariKiran <harikiran.kante@gmail.com>
2022-10-25 22:42:11 +0530

    added premium courses section

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -2,6 +2,8 @@ const hamBtn = document.querySelector(".fa-bars");
 const navItemsContainer = document.querySelector(".nav-items");
 const navItems = document.querySelectorAll(".nav-link");
 const courses = document.querySelector(".courses");
+const premium = document.querySelector(".premium-courses");
+const offerings = document.querySelector(".offerings");
 // Event Listeners
 
 hamBtn.onclick = function () {
@@ -56,6 +58,47 @@ window.onload = function () {
         });
         div.appendChild(cards);
         courses.appendChild(div);
+      });
+    });
+  fetch("./assests/data/premium-courses.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((c) => {
+        const pcard = document.createElement("a");
+        pcard.href = c["course-link"];
+        pcard.classList.add("course-card");
+        pcard.innerHTML = `
+        <div class="course-icon">
+            <img src=${c["course-icon"]} alt="Logo" />
+          </div>
+          <div class="course-name">${c["course-name"]}</div>
+        `;
+        premium.appendChild(pcard);
+      });
+    });
+  fetch("./assests/data/premium-offerings.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((el) => {
+        const offCard = document.createElement("div");
+        offCard.classList.add("offering-card");
+        offCard.innerHTML = `
+        <div class="icon">
+            <img
+              src="${el["icon"]}"
+              alt="icon"
+            />
+          </div>
+          <div class="title">${el["title"]}</div>
+          <div class="desc">
+          ${el["desc"]}
+          </div>
+          `;
+        offerings.appendChild(offCard);
       });
     });
 };
